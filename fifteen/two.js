aa5408fd9822c5b993c3e9c883e8a9ba1fecd21f
Mackinder, Patrick <paddymackinder@gmail.com>
2016-12-18 18:07:39 +0000

    feat: Completed day fifteen part 2. PM

---
+++ fifteen/two.js
@@ -0,0 +1,17 @@
+const fs = require("fs"),
+      Disk = require('./Disk').Disk;
+
+fs.readFile("fifteen/input.txt", "utf8", function(err, data) {
+    if(err) {return console.log(err);}
+
+    const disks = data.split('\n')
+        .map(string => Disk.create(string))
+        .concat([new Disk(11, 0, 0)]);
+
+    let i = 0;
+
+    while(disks.some((disk, index) => !disk.isOpen(i+index+1))) {
+        i++;
+    }
+    console.log(i);
+});
