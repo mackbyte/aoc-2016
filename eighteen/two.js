f7b03ca944f8b5386dde3c9686587bb318cef2ac
Mackinder, Patrick <paddymackinder@gmail.com>
2016-12-18 21:54:43 +0000

    feat: Completed day eighteen part 2. PM

---
+++ eighteen/two.js
@@ -0,0 +1,12 @@
+const fs = require("fs"),
+      Room = require('./Room').Room;
+
+fs.readFile("eighteen/input.txt", "utf8", function(err, data) {
+    if(err) {return console.log(err);}
+
+    const room = new Room(data);
+
+    room.generate(400000);
+
+    console.log(`Safe tiles: ${room.safeTiles()}`);
+});
